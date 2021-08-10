a2473cf31874082e2a0c013281c477f91cdbd6d2
devanshdwivedi <devanshdwivedi@gmail.com>
2021-08-10 15:06:09 +0530

    Adding lanes

--- src/ActivityBoard.tsx
+++ src/ActivityBoard.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Lane from "./components/Lane";
 
 function ActivityBoard() {
-  return <div>This will be an activity tracker</div>;
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <Lane activityState={"New"} canShrink={true} />
+      <Lane activityState={"Committed"} />
+      <Lane activityState={"In progress"} />
+      <Lane activityState={"In review"} />
+      <Lane activityState={"Done"} canShrink={true} />
+    </div>
+  );
 }
 
 export default ActivityBoard;
---
+++ src/components/Lane.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+
+interface LaneProps {
+  activityState: string;
+  canShrink?: boolean;
+}
+
+interface LaneHeaderProps {
+  title: string;
+  canShrink?: boolean;
+  expandCallback: any;
+  expanded?: boolean;
+}
+
+function LaneHeader({
+  title,
+  canShrink,
+  expandCallback,
+  expanded,
+}: LaneHeaderProps) {
+  return (
+    <div
+      style={{
+        borderBottom: expanded ? "2px solid black" : "",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      {expanded ? <div style={{ margin: "0 0 0 10px" }}>{title}</div> : null}
+      {canShrink ? (
+        <div
+          onClick={expandCallback}
+          style={{ cursor: "pointer", padding: "0 10px" }}
+        >
+          {expanded ? "<" : ">"}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
+export default function Lane({ activityState, canShrink }: LaneProps) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(true);
+  const expandCallback = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const contractedMarkup = () => {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          width: "30px",
+          margin: "2px",
+          border: "1px solid black",
+        }}
+      >
+        <LaneHeader
+          expanded={isExpanded}
+          expandCallback={expandCallback}
+          canShrink={canShrink}
+          title={activityState}
+        />
+        {
+          <div
+            style={{
+              paddingTop: "30px",
+              writingMode: "vertical-rl",
+              textOrientation: "mixed",
+              position: "relative",
+              left: "5px",
+            }}
+          >
+            {activityState}
+          </div>
+        }
+      </div>
+    );
+  };
+
+  const expandedMarkup = () => {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          flex: "1",
+          margin: "2px",
+          border: "1px solid black",
+        }}
+      >
+        <LaneHeader
+          expanded={isExpanded}
+          expandCallback={expandCallback}
+          canShrink={canShrink}
+          title={activityState}
+        />
+      </div>
+    );
+  };
+
+  return isExpanded ? expandedMarkup() : contractedMarkup();
+}
