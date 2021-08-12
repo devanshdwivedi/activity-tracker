31e98c74c115796abc0d704f302bfe0f6144b2bc
devanshdwivedi <devanshdwivedi@gmail.com>
2021-08-12 22:10:43 +0530

    Prettier

--- src/components/NewActivity.tsx
+++ src/components/NewActivity.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 
 interface NewActivityProps {
   isOpen: boolean;
@@ -11,15 +11,14 @@ export default function NewActivity({
   isOpen,
   closeCallback,
 }: NewActivityProps) {
-
   const formik = useFormik({
     initialValues: {
-        activityTitle: 'New Activity',
-        priority: "3"
+      activityTitle: "New Activity",
+      priority: "3",
     },
-    onSubmit: values => {
-        console.log(values);
-    }
+    onSubmit: (values) => {
+      console.log(values);
+    },
   });
 
   const customStyles = {
@@ -37,24 +36,30 @@ export default function NewActivity({
     <Modal isOpen={isOpen} onRequestClose={closeCallback} style={customStyles}>
       <form onSubmit={formik.handleSubmit}>
         <div>
-            <label htmlFor="activityTitle">Event Title</label>
-            <input id="activityTitle" name="activityTitle" type="text" onChange={formik.handleChange} value={formik.values.activityTitle} />
+          <label htmlFor="activityTitle">Event Title</label>
+          <input
+            id="activityTitle"
+            name="activityTitle"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.activityTitle}
+          />
         </div>
         <div>
-            <label htmlFor="priority">Priority</label>
-            <select
-                id="priority"
-                name="priority"
-                value={formik.values.priority}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-            >
-                <option value={1} label="1" />
-                <option value={2} label="2" />
-                <option value={3} label="3" />
-                <option value={4} label="4" />
-                <option value={5} label="5" />
-            </select>
+          <label htmlFor="priority">Priority</label>
+          <select
+            id="priority"
+            name="priority"
+            value={formik.values.priority}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          >
+            <option value={1} label="1" />
+            <option value={2} label="2" />
+            <option value={3} label="3" />
+            <option value={4} label="4" />
+            <option value={5} label="5" />
+          </select>
         </div>
         <button type="submit">Create Activity</button>
       </form>
